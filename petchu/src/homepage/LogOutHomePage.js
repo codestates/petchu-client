ae82c81becdb042f0292149b0f2850908fad3ab1
임희찬 <imhuichan@imhuichan-ui-MacBookPro.local>
2020-11-13 17:04:23 +0900

    fix all pages

---
+++ petchu/src/homepage/LogOutHomePage.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link, Switch,Route, Redirect, withRouter } from "react-router-dom";
+import axios from "axios";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import logo from '../images/logo.png';
+
+axios.defaults.withCredentials = true;
+
+class LogOutHomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    render() {
+        return( 
+            <div>
+                <header>
+                <Link to="/">
+                    <button>
+                    <img className="logo" src={logo} width="13" height="14" alt="띠용"></img>    
+                    </button>
+                </Link>
+                
+                <Link to="/signin"><button>Sign In</button></Link>
+                <Link to="/signup"><button>Sign Up</button></Link>
+                </header>
+                <hr />
+                <main>
+                    <Switch>
+                        <Route exact path="/"></Route>
+                        <Route path="/signin"
+                            render={() => {
+                              return <SignIn handleResponseSuccess={this.props.handleResponseSuccess}/>
+                            }}
+                        ></Route>
+                        <Route path="/signup"
+                            render={() => {
+                                return <SignUp/>
+                            }}
+                        ></Route>
+                    </Switch>
+                </main>
+            </div>
+                
+            
+        );
+    };
+}
+
+export default withRouter(LogOutHomePage);
